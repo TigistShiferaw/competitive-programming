61f1e7fea25ca213146789e0087a5c9a14ae202a
TigistShiferaw <tgg909441@gmail.com>
2023-02-06 13:40:06 +0300

    2/6/2023

---
+++ squid game/week2/2185. Counting Words With a Given Prefix.py
@@ -0,0 +1,7 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        ans = 0
+        for i in range(len(words)):
+            if len(words[i]) >= len(pref) and words[i][:len(pref)] == pref:
+                ans += 1
+        return ans
